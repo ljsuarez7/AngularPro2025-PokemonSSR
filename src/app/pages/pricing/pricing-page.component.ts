ef60461ef8b52101674e41369e4959942e61777d
JaviSrz <ljsuarez7@gmail.com>
2025-06-15 16:18:51 +0200

    Ejemplo de como usar codigo que solo se ejecute en el server o solo en el navegador

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -11,8 +12,16 @@ export default class PricingPageComponent implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  // private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+
+    //Esto no cambia el titulo que se envia al navegador en el html
+    // if(!isPlatformServer(this.platform)) {
+    // if(isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing page';
+    // }
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({name: 'description', content: 'Este es mi pricing page'});
     this.meta.updateTag({name: 'og:title', content: 'Pricing Page'});
