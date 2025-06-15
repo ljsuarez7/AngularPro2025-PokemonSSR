79b5e8ecc011062e1a604c389d6a7d483619d947
JaviSrz <ljsuarez7@gmail.com>
2025-06-15 09:25:21 +0200

    página contact con el contenido minimo

---
+++ src/app/pages/contact/contact-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ContactPageComponent { }
