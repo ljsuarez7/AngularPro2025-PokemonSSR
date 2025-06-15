63a01eaaf62d268217e56c464684722a33629dc7
JaviSrz <ljsuarez7@gmail.com>
2025-06-15 09:24:45 +0200

    página about con el contenido minimo

---
+++ src/app/pages/about/about-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AboutPageComponent { }
