c9e28a9ddb3aa04e76ef81f5becb5a05365089ad
JaviSrz <ljsuarez7@gmail.com>
2025-06-15 09:25:03 +0200

    página pricing con el contenido minimo

---
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPageComponent { }
