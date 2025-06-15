8bc7378643ee541afd5659b248ae4fe173dbe50a
JaviSrz <ljsuarez7@gmail.com>
2025-06-15 16:18:14 +0200

    Añadimos el routerlinkactive para marcar en el navbar la pagina actual

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
