5a9880e40989ef8d14e4c972f5248bbd1e78e6cf
JaviSrz <ljsuarez7@gmail.com>
2025-10-13 11:26:40 +0200

    Arreglo para el despliegue en production

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,10 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'pokemons/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
