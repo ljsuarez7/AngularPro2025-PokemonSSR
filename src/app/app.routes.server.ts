33ec4164a186dedec671fd7fd369fc92416359bc
JaviSrz <ljsuarez7@gmail.com>
2025-10-14 20:05:52 +0200

    arreglo para desplegar en pro

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'pokemons/:id',
     renderMode: RenderMode.Client
   },
