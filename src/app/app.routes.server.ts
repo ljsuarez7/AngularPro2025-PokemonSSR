aeb4d434eac905f1570a7e59a6bf1713dd890f91
JaviSrz <ljsuarez7@gmail.com>
2025-10-19 13:45:03 +0200

    Prerenderizamos las primeras 5 paginas del listado y las 10 primeras de detalle de pokemon por id

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,16 +1,28 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+const TOTAL_POKEMONS = 10;
+
+//TODO: Investigar como hacer el prerender en angular 20. Por ejemplo prerenderizar las 10 primeras paginas del listado de pokemon o alguna paginas de detalle de pokemon
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'pokemons/page/:page',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      const pages = ['1', '2', '3', '4', '5'];
+      return Promise.resolve(pages.map((page) => ({page})));
+    }
   },
   {
     path: 'pokemons/:id',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return Array.from({ length: TOTAL_POKEMONS}, (_, index) => ({
+        id: (index + 1).toString()
+      }))
+    }
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
